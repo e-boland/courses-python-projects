5e826b3d20f1decc235e4e4d0fabacbd97808b00
e-boland <evaboland@hotmail.com>
2024-01-16 14:57:45 +0100

    [ADD] Docstrings in module m_user

--- m_user.py
+++ m_user.py
@@ -1,13 +1,28 @@
+"""Module to generate random users"""
+
 import faker
 
 fake = faker.Faker()
 
 
 def get_user():
+    """ Generate a single user
+
+    Returns:
+        str: user
+    """
     return f"{fake.first_name()} {fake.last_name()}"
 
 
 def get_users(n: int):
+    """ Generate a list of users
+
+    Args:
+        n (): int: number of users to generate
+
+    Returns: list[str]: users
+
+    """
     return [get_user() for _ in range(n)]
 
 
